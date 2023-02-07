02c12af49ec39e41179c5917d22cabf15445de59
Vismaya <vismayavipz999@gmail.com>
2023-02-07 13:21:27 +0530

    searIcon in header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grid, TextField, Button } from "@material-ui/core";
+import {
+  Container,
+  Grid,
+  TextField,
+  Button,
+  AppBar,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import { MovieCard } from "./components/MovieCard.js";
-import Header from "./components/Header.js";
-import Footer from "./components/Footer.js"
-// import MovieCategory from "./components/MoviesByCategory.js";
-
+import Footer from "./components/Footer.js";
 const API_KEY = "c8082b36";
-
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
@@ -20,7 +24,7 @@ const App = () => {
       );
       const data = await res.json();
       console.log(data.Search);
-      setMovies(data.Search);
+      setMovies(data.Search.filter((obj) => obj.Poster !== "N/A"));
     };
     fetchMovies();
   }, [category]);
@@ -28,7 +32,6 @@ const App = () => {
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
   };
-
 
   const filteredMovies = movies
     ? movies.filter((movie) =>
@@ -38,34 +41,31 @@ const App = () => {
 
   return (
     <Container>
-      <Header/>
-      {/* <h1 className="text-center my-10">Movie List</h1> */}
-      <TextField
-        label="Search"
-        type="text"
-        value={searchTerm}
-        onChange={handleSearch}
-      />
-      <Button variant="outlined" onClick={() => setCategory(searchTerm)}>
-        Search
-      </Button>
-      <Button variant="outlined"  onClick={() => setCategory('trending')}>
-        Trending
-      </Button>
-
-     
-      <Grid container spacing={3} alignItems = 'center'>
+      <AppBar color="white">
+        <Toolbar>
+          <Typography variant="h6">Movies</Typography>
+          <TextField
+            label="Search"
+            type="text"
+            value={searchTerm}
+            onChange={handleSearch}
+          />
+          <Button variant="outlined" onClick={() => setCategory(searchTerm)}>
+            Search
+          </Button>
+          <Button variant="outlined" onClick={() => setCategory("trending")}>
+            Trending
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Grid container spacing={3} alignItems="center">
         {filteredMovies.map((movie) => (
           <Grid item key={movie.imdbID} xs={12} sm={6} md={4} lg={3}>
             <MovieCard movie={movie} />
           </Grid>
         ))}
-         
       </Grid>
-      
-      <Footer/>
-    
-      
+      <Footer />
     </Container>
   );
 };
